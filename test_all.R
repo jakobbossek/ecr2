184bb31a22dd64ab303312de98365481690595a7
Jakob Bossek <info@jakobbossek.de>
2017-01-18 13:54:09 +0100

    update test files

--- test_all.R
+++ test_all.R
@@ -5,7 +5,7 @@ library(testthat)
 if (interactive()) {
   load_all(".")
 } else {
-  library(ecr)
+  library(ecr2)
 }
 
 test_dir("tests/testthat")
--- tests/test_all.R
+++ tests/test_all.R
@@ -1,3 +1,3 @@
 library(testthat)
-test_check("ecr")
+test_check("ecr2")
 

7999f8d071e6e3a6c9df72dc62376e5207a501c8
Jakob Bossek <info@jakobbossek.de>
2017-01-19 22:54:32 +0100

    new interface for crossover

--- R/operator.recombinator.crossover.R
+++ R/operator.recombinator.crossover.R
@@ -12,54 +12,26 @@
 #' If the parents are list of real-valued/binary vectors, the procedure described
 #' above is applied to each element of the list.
 #'
-#' @param p [\code{numeric(1)}]\cr
-#'   Cross over probability to form an offspring. Default is \code{1}.
 #' @return [\code{ecr_recombinator}]
 #' @family recombinators
 #' @export
-setupCrossoverRecombinator = function(p = 1) {
-  assertNumber(p, lower = 0, upper = 1)
-  force(p)
-
-  recombinator = function(inds, task, control) {
-    par.set = task$par.set
-    n.params = getParamLengths(par.set)
-    # we have to make sure, that the gene has length > 1. This should not
-    # be the case in pratice use, but it causes errors
-    if (min(n.params) <= 1L) {
-      stopf("Crossover recombinator requires genes to have length > 1.")
-    }
-
-    # do a cross-over or not
-    if (runif(1L) >= p) {
-      return(wrapChildren(inds[[1]], inds[[2]]))
-    }
+setupCrossoverRecombinator = function() {
+  recombinator = function(inds) {
+    n = length(inds[[1L]])
 
     # recombinate sub genes
-    recombGenes = function(parent1, parent2, n) {
-      idx = sample(seq(n - 1), size = 1L)
-      # back part from other parent
-      child1 = parent2
-      child2 = parent1
-      # front part from "their" parent
-      child1[1:idx] = parent1[1:idx]
-      child2[1:idx] = parent2[1:idx]
+    parent1 = inds[[1L]]
+    parent2 = inds[[2L]]
 
-      return(list(child1, child2))
-    }
+    idx = sample(seq(n - 1), size = 1L)
+    # back part from other parent
+    child1 = parent2
+    child2 = parent1
+    # front part from "their" parent
+    child1[1:idx] = parent1[1:idx]
+    child2[1:idx] = parent2[1:idx]
 
-    # overwrite individuals with offsprings
-    if (getParamNr(par.set) == 1L) {
-      inds = recombGenes(inds[[1]], inds[[2]], n.params)
-    } else {
-      for (i in seq(getParamNr(par.set))) {
-        children = recombGenes(inds[[1]][[i]], inds[[2]][[i]], n.params[i])
-        inds[[1]][[i]] = children[[1]]
-        inds[[2]][[i]] = children[[2]]
-      }
-    }
-    # return two offsprings
-    return(wrapChildren(inds[[1]], inds[[2]]))
+    return(wrapChildren(child1, child2))
   }
 
   makeRecombinator(
@@ -68,7 +40,6 @@ setupCrossoverRecombinator = function(p = 1) {
     description = "Performs classical one-point crossover.",
     n.parents = 2L,
     supported = c("float", "binary"),
-    params = list(p = p),
     n.children = 2L
   )
 }

2ff390da0348c95b7e437fb2f30179b03b6bd0a8
Jakob Bossek <info@jakobbossek.de>
2017-01-26 16:48:12 +0100

    add mutate S3 helper

--- R/generateOffspring.R
+++ R/generateOffspring.R
@@ -75,3 +75,56 @@ generateOffspring = function(control, population, fitness, lambda, p.recomb = 0.
 
   return(offspring)
 }
+
+#' @title Mutate helper.
+#'
+#' @description Receives a list of individuals and a control object and applies
+#' the mutation operator stored in control to each individual with a certain
+#' probability.
+#'
+#' @param x [\code{ecr2_control} | \code{ecr2_mutator}]\cr
+#'   Either a mutator or a control object. If a control object is passed
+#'   the functions is internally called on the mutator in \code{control$mutate}.
+#'   In this case the \code{control$params} list is passed down via \code{par.list}.
+#' @param inds [\code{list}]\cr
+#'   List of individuals.
+#' @template arg_p_mut
+#' @param par.list [\code{list}]\cr
+#'   Named list of parameters passed down to mutator.
+#'   Default is the empty list.
+#' @param ... [any]\cr
+#'   Further parameters passed down to mutators.
+#'   There parameters will overwrite parameters in \code{par.list}.
+#' @return [\code{list}] Mutated \code{inds} list.
+#' @export
+#' @examples
+#' # generate list of bistrings and apply mutation
+#' inds = replicate(5, sample(c(0, 1), 10, replace = TRUE), simplify = FALSE)
+#' mutator = setupBitflipMutator(p = 0.1)
+#' mut.inds = mutate(mutator, inds, p.mut = 0.4)
+mutate = function(x, inds, p.mut, par.list = list(), ...) {
+  UseMethod("mutate")
+}
+
+#' @export
+mutate.ecr2_mutator = function(x, inds, p.mut, par.list = list(), ...) {
+  assertNumber(p.mut, lower = 0, upper = 1)
+  assertList(inds)
+  assertList(par.list)
+  par.list = BBmisc::insert(par.list, list(...))
+  mutatorFun = x
+  do.mutate = runif(length(inds)) < p.mut
+  if (any(do.mutate > 0)) {
+    inds[do.mutate] = lapply(inds[do.mutate], mutatorFun, par.list)
+  }
+  return(inds)
+}
+
+#' @export
+mutate.ecr2_control = function(x, inds, p.mut = 0.3, par.list = list(), ...) {
+  if (!is.null(x$mutate)) {
+    par.list = BBmisc::insert(x$params, par.list)
+    return(mutate(x$mutate, inds, p.mut, par.list, ...))
+  }
+  return(inds)
+}

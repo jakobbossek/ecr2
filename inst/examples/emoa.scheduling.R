dcc994f0753c2b5eb5060f70468db66d64c1f079
Jakob Bossek <info@jakobbossek.de>
2017-03-27 23:46:09 +0200

    update/finalize emoa.scheduling example

--- inst/examples/emoa.scheduling.R
+++ inst/examples/emoa.scheduling.R
@@ -2,6 +2,24 @@ library(devtools)
 
 load_all(".")
 
+# AN EMOA FOR A BI-OBJECTIVE SCHEDULING PROBLEM
+# =============================================
+# Here we aim to approximate the Pareto optimal front of the 1||Sum C_i, L_max
+# scheduling problem. A schedule is a permutation of jobs/tasks. We are searching
+# for a job order which minimizes the sum of completion times Sum C_i and
+# simultaneously minimizes the maximal lateness L_max = max (C_i - d_i).
+# Since both objectives are conflicting we can not expect to find a single, optimal
+# solution. We rather search the search space for a set of incomparable trade-off
+# solutions.
+#
+# We do so by applying a (20 + 10)-EMOA for 2000 generations. Since we are searching
+# for job orders, each individual is a permutation of the set {1, ..., n} where n
+# is the number of jobs. We can thus rely on standard mutation and recombination for
+# this kind of genotype: scramble mutation and ordered crossover.
+# Nondominated sorting eventually followed by crowding distance computation is employed
+# as the multi-criteria survival selection mechanism.
+
+# reproducability
 set.seed(1)
 
 # setup
@@ -10,7 +28,7 @@ n.jobs = 25L
 # generate random processing times and due dates
 proc.times = runif(n.jobs, 1, 20)
 due.dates = proc.times + sapply(proc.times, function(i) {
-  rnorm(1L, mean = 10 * i, sd = 9 * i / 3)
+  rnorm(1L, mean = 10 * i, sd = 3 * i)
 })
 # store jobs in data.frame
 jobs = data.frame(j = 1:n.jobs, p = proc.times, d = due.dates)
@@ -36,7 +54,7 @@ control = registerECROperator(control, "selectForSurvival", setupNondomSelector(
 
 # initialize population of random schedules
 population = genPerm(MU, n.jobs)
-fitness = evaluateFitness(population, control)
+fitness = evaluateFitness(control, population)
 
 # initialize logger (store HV)
 log = initLogger(control,
@@ -53,7 +71,7 @@ for (i in seq_len(MAX.ITER)) {
   offspring = mutate(control, offspring, p.mut = 0.3)
 
   # calculate costs of new schedules
-  fitness.o = evaluateFitness(offspring, control)
+  fitness.o = evaluateFitness(control, offspring)
 
   # apply (MU + LAMBDA) selection
   sel = replaceMuPlusLambda(control, population, offspring, fitness, fitness.o)
@@ -65,6 +83,8 @@ for (i in seq_len(MAX.ITER)) {
 }
 
 stats = getStatistics(log)
-pl.stats = plotStatistics(stats)
-pl.front = plotFront(fitness, obj.names = c("SumCj", "Lmax"))
-print(pl.front)
+pl.stats = plotStatistics(stats) + theme(legend.position = "top")
+# ggsave("emoa_scheduling_statistics.pdf", width = 8, height = 4, plot = pl.stats)
+# pl.front = plotFront(fitness, obj.names = c("SumCi", "Lmax"))
+# ggsave("emoa_scheduling_front.pdf", width = 4, height = 4, plot = pl.front)
+# print(pl.front)

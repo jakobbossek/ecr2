e75a0903c4e0e3f9f5c634721022d742a3b06515
Jakob Bossek <info@jakobbossek.de>
2017-12-19 12:28:15 +0100

    add driver arg to plotScatter3d (scatterplot3d, plot3D already work)

--- R/plotFronts.R
+++ R/plotFronts.R
@@ -1,4 +1,4 @@
-#' @title Visualize Pareto-front approximations.
+#' @title Visualize bi-objective Pareto-front approximations.
 #'
 #' @description Given a data frame with the results of (multiple) runs of (multiple)
 #' different multi-objective optimization algorithms on (multiple) problem instances
@@ -132,6 +132,82 @@ plotFronts = function(df, obj.cols = c("f1", "f2"), shape = "algorithm", colour 
   return(pl)
 }
 
+#' @title Visualize three-objective Pareto-front approximations.
+#'
+#' @description Given a data frame with the results of (multiple) runs of (multiple)
+#' different three-objective optimization algorithms on (multiple) problem instances
+#' the function generates 3D scatterplots of the obtained Pareto-front approximations.
+#'
+#' @param df [\code{data.frame}]\cr
+#'   Data.frame with columns at least \code{obj.cols}, \dQuote{prob} and \dQuote{algorithm}.
+#' @param obj.cols [\code{character(>= 3)}]\cr
+#'   Column names of the objective functions.
+#'   Default is \code{c("f1", "f2", "f3")}.
+#' @param max.in.col [\code{integer(1)}]\cr
+#'   Maximum number of plots to be displayed side by side.
+#'   Default is 4.
+#' @param ... [any]\cr
+#'   Furhter parameters passed down to plot function.
+#' @return Nothing
+#' @export
+plotFronts3d = function(df, obj.cols = c("f1", "f2", "f3"), max.in.col = 4L, driver = "scatterplot3d", ...) {
+  assertDataFrame(df, min.rows = 2L, min.cols = 5L)
+  assertCharacter(obj.cols, min.len = 2L)
+  assertChoice(driver, c("scatterplot3d", "plot3D", "plotly"))
+  max.in.col = asInt(max.in.col, lower = 1L, upper = 10L)
+
+  if (!all(obj.cols %in% colnames(df)))
+    stopf("obj.cols needs to contain valid column names.")
+
+  # get algorithm and problem names
+  algos = factor(unique(df$algorithm))
+  probs = factor(unique(df$prob))
+
+  # how many algorithms/problems are we faced with?
+  n.algos = nlevels(algos)
+  n.probs = nlevels(probs)
+
+  # points types and colours
+  #FIXME: I assume that we need to do this for each driver in another way
+  pchs = 1:16
+  cols = viridis::viridis_pal()(n.probs)
+
+  n.rows = ceiling(n.probs / max.in.col)
+  opar = graphics::par(mfrow = c(n.rows, n.probs))
+  on.exit(par(opar))
+
+  #FIXME: enable driver
+  for (prob in probs) {
+    df2 = df[df$prob == prob, , drop = FALSE]
+    prob.enc = as.integer(as.factor(df2$algorithm))
+    pchs2 = pchs[prob.enc]
+    cols2 = cols[prob.enc]
+
+    if (driver == "scatterplot3d") {
+      scatterplot3d::scatterplot3d(x = as.matrix(df2[, obj.cols]),
+        pch = pchs2, color = cols2, main = prob, ...)
+      graphics::legend("topright", legend = algos, bg = "white", col = cols[as.integer(algos)],
+        pch = pchs[as.integer(algos)])
+    } else if (driver == "plot3D") {
+      x = df2[, obj.cols[1L]]
+      y = df2[, obj.cols[2L]]
+      z = df2[, obj.cols[3L]]
+      objs = list(x = x, y = y, z = z)
+      args.default = list(
+        colvar = prob.enc, col = cols,
+        pch = pchs2,
+        ticktype = "detailed",
+        colkey = list(
+          at = 1:n.algos,
+          labels = algos
+        ),
+        bty = "b2")
+      args = c(objs, BBmisc::insert(args.default, list(...)))
+      do.call(plot3D::scatter3D, args)
+    }
+  }
+}
+
 #FIXME: docs
 convertToGG = function(x, value.name = "value") {
   rns = rownames(x)
@@ -221,5 +297,34 @@ plotHeatmap = function(x, value.name = "Value", show.values = FALSE) {
     axis.text.x = element_text(angle = 45, hjust = 1),
     legend.position = "top")
   pl = pl + ggplot2::xlab("") + ggplot2::ylab("")
+  return(pl)
+}
+
+#' @title Plot distributions of EMOA indicators.
+#'
+#' @description Visualizes of empirical distributions of unary EMOA indicator
+#' based on the results of \code{\link{compareApproximations}}.
+#'
+#' @param inds [\code{data.frame}]\cr
+#'   Data frame with columns \dQuote{algo}, \dQuote{prob}, \dQuote{repl} and
+#'   one additional column per EMOA indicator.
+#' @param plot.type [\code{character(1)}]\cr
+#'   Either \dQuote{boxplot} (the default) for boxplots or \dQuote{violin} for
+#'   violin plots.
+#' @return [\code{\link[ggplot2]{ggplot}}]
+plotIndicatorDistribution = function(inds) {
+  df = reshape2::melt(inds, id.vars = c("algo", "prob", "repl"), value.name = "Value", variable.name = "Measure")
+  print(head(df))
+  pl = ggplot2::ggplot(df, ggplot2::aes_string(x = "algo", y = "Value"))
+  pl = pl + ggplot2::geom_boxplot(ggplot2::aes_string(fill = "Measure"))
+  pl = pl + ggplot2::facet_grid(Measure ~ prob)
+
+  # pl = ggplot(df, aes_string(x = "prob", y = "Value"))
+  # pl = pl + geom_boxplot(aes_string(fill = "algo"))
+  # #pl = pl + facet_wrap("Measure")
+  # pl = pl + facet_grid(algo ~ .)
+  pl = pl + ggplot2::theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  pl = pl + ggplot2::scale_y_log10()
+  pl = pl + viridis::scale_fill_viridis(discrete = TRUE)
   return(pl)
 }

96aca3743f8d2426577dcc16f15006d1ebaa11ca
Jakob Bossek <info@jakobbossek.de>
2017-12-19 14:23:07 +0100

    plotScatter3d now supports plotly (need to fix default zoom level)

--- R/plotScatter3d.R
+++ R/plotScatter3d.R
@@ -24,7 +24,7 @@
 plotScatter3d = function(df, obj.cols = c("f1", "f2", "f3"), max.in.col = 4L, package = "scatterplot3d", ...) {
   assertDataFrame(df, min.rows = 2L, min.cols = 5L)
   assertCharacter(obj.cols, min.len = 2L)
-  assertChoice(package, c("scatterplot3d", "plot3D", "plot3Drgl"))
+  assertChoice(package, c("scatterplot3d", "plot3D", "plot3Drgl", "plotly"))
   max.in.col = asInt(max.in.col, lower = 1L, upper = 10L)
 
   if (!all(obj.cols %in% colnames(df)))
@@ -49,46 +49,78 @@ plotScatter3d = function(df, obj.cols = c("f1", "f2", "f3"), max.in.col = 4L, pa
   opar = graphics::par(mfrow = c(n.rows, n.probs))
   on.exit(graphics::par(opar))
 
-  #FIXME: enable package
-  for (prob in probs) {
-    df2 = df[df$prob == prob, , drop = FALSE]
-    prob.enc = as.integer(as.factor(df2$algorithm))
-    pchs2 = pchs[prob.enc]
-    cols2 = cols[prob.enc]
+  if (package %in% c("scatterplot3d", "plot3D", "plot3Drgl")) {
+    for (prob in probs) {
+      df2 = df[df$prob == prob, , drop = FALSE]
+      prob.enc = as.integer(as.factor(df2$algorithm))
+      pchs2 = pchs[prob.enc]
+      cols2 = cols[prob.enc]
 
-    if (package == "scatterplot3d") {
-      objs = list(x = as.matrix(df2[, obj.cols]))
-      args.default = list(
-        pch = pchs2, color = cols2,
-        main = prob)
-      args = c(objs, BBmisc::insert(args.default, list(...)))
-      do.call(scatterplot3d::scatterplot3d, args)
-      graphics::legend("topright", legend = algos, bg = "white", col = cols[as.integer(algos)],
-        pch = pchs[as.integer(algos)])
-    } else if (package %in% c("plot3D", "plot3Drgl")) {
-      x = df2[, obj.cols[1L]]
-      y = df2[, obj.cols[2L]]
-      z = df2[, obj.cols[3L]]
-      objs = list(x = x, y = y, z = z)
-      args.default = list(
-        colvar = prob.enc, col = cols,
-        pch = pchs2,
-        ticktype = "detailed", # we want to see axis ticks
-        colkey = list( # labeled legend
-          at = 1:n.algos,
-          labels = algos
-        ),
-        bty = "g", #"b2", # layout of the box
-        main = prob,
-        xlab = obj.cols[1L],
-        ylab = obj.cols[2L],
-        zlab = obj.cols[3L],
-        clab = "Algorithm" # label for legend
-      )
-      args = c(objs, BBmisc::insert(args.default, list(...)))
-      do.call(plot3D::scatter3D, args)
-      if (package == "plot3Drgl")
-        plot3Drgl::plotrgl()
+      if (package == "scatterplot3d") {
+        objs = list(x = as.matrix(df2[, obj.cols]))
+        args.default = list(
+          pch = pchs2, color = cols2,
+          main = prob)
+        args = c(objs, BBmisc::insert(args.default, list(...)))
+        do.call(scatterplot3d::scatterplot3d, args)
+        graphics::legend("topright", legend = algos, bg = "white", col = cols[as.integer(algos)],
+          pch = pchs[as.integer(algos)])
+      } else if (package %in% c("plot3D", "plot3Drgl")) {
+        x = df2[, obj.cols[1L]]
+        y = df2[, obj.cols[2L]]
+        z = df2[, obj.cols[3L]]
+        objs = list(x = x, y = y, z = z)
+        args.default = list(
+          colvar = prob.enc, col = cols,
+          pch = pchs2,
+          ticktype = "detailed", # we want to see axis ticks
+          colkey = list( # labeled legend
+            at = 1:n.algos,
+            labels = algos
+          ),
+          bty = "g", #"b2", # layout of the box
+          main = prob,
+          xlab = obj.cols[1L],
+          ylab = obj.cols[2L],
+          zlab = obj.cols[3L],
+          clab = "Algorithm" # label for legend
+        )
+        args = c(objs, BBmisc::insert(args.default, list(...)))
+        do.call(plot3D::scatter3D, args)
+        if (package == "plot3Drgl")
+          plot3Drgl::plotrgl()
+      } else {
+        stopf("plotScatter3d: this should not happen. Please contact the package author.")
+      }
     }
+  } # endif (package %in% ...)
+  if (package == "plotly") {
+    i = 1
+    plot.list = lapply(probs, function(prob) {
+      df2 = df[df$prob == prob, , drop = FALSE]
+      prob.enc = as.integer(as.factor(df2$algorithm))
+
+      x = df2[[obj.cols[1L]]]
+      y = df2[[obj.cols[2L]]]
+      z = df2[[obj.cols[3L]]]
+
+      pl = plotly::plot_ly(df2, x = x, y = y, z = z,
+        color = df2$algorithm, colors = cols, scene = paste0("scene", i))
+      i <<- i + 1L
+
+      pl = plotly::add_markers(pl)
+      pl = plotly::layout(pl, title = prob)
+      return(pl)
+    })
+
+    plot.list$nrows = n.rows
+    p = do.call(plotly::subplot, plot.list)
+
+    scene.options = list()
+    for (i in 1:n.probs) {
+      scene.options[[paste0("scene", i)]] = list(aspectmode = "cube")
+    }
+    p = do.call(plotly::layout, c(list(p = p), scene.options))
+    print(p)
   }
 }

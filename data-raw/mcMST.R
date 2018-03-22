dda62d5aa937218d3fcaca188f3947a57cc1aa98
Jakob Bossek <info@jakobbossek.de>
2018-03-22 16:00:04 -0700

    reduce mcMST dataset size -> now it does not exceed 5MB anymore

--- data-raw/mcMST.R
+++ data-raw/mcMST.R
@@ -2,11 +2,18 @@ library(tidyverse)
 
 mcMST = readr::read_delim("data-raw/mcMST.csv", delim = " ")
 
-# remove the following lines
-#mcMST = mcMST[grepl("instance-100", mcMST$prob.id), , drop = FALSE]
-#mcMST = subset(mcMST, emoatype == "NSGA2")
-mcMST$mutator = recode(mcMST$mutator, uniform = "ZHOU",
-  edgeExchange = "EX", subgraphMST = "SG", mixed = "MIXED")
+# 5 MB data should not be exceeded (https://cran.r-project.org/web/packages/policies.html)
+# -> filter first three instances of size 40, 50 and 100 respectively
+mcMST = mcMST[grepl("instance-(040|050|100)*-[1-3]*$", mcMST$prob.id, perl = TRUE), , drop = FALSE]
+
+# rename stuff
+mcMST$mutator = recode(mcMST$mutator,
+  uniform = "ZHOU",
+  edgeExchange = "EX",
+  subgraphMST = "SG",
+  mixed = "MIXED")
+
+# collapse meta heuristic and mutation operator
 mcMST$algorithm = paste(mcMST$emoatype, mcMST$mutator, sep = ".")
 mcMST$algo.id = mcMST$emoatype = mcMST$mutator = NULL
 print(head(mcMST))

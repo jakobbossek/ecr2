86ebf43d2e3bec5dff7ead3fa6f9f07107cae223
Jakob Bossek <info@jakobbossek.de>
2019-03-02 18:28:40 +1030

    fix issue in selDomHV not informing the user that n.select needs to be equal to (ncol(fitness) - 1)

--- R/operator.selector.dominatedHypervolume.R
+++ R/operator.selector.dominatedHypervolume.R
@@ -2,8 +2,14 @@
 #' Dominated Hypervolume selector.
 #'
 #' @description
-#' Performs nondominated sorting and drops the individual from the last front
-#' with minimal hypervolume contribution.
+#' Performs non-dominated sorting and drops the individual from the last front
+#' with minimal hypervolume contribution. This selector is the basis of the
+#' S-Metric Selection Evolutionary Multi-Objective Algorithm, termed SMS-EMOA
+#' (see \code{\link{smsemoa}}).
+#'
+#' @note Note that the current implementation expects \code{n.select = ncol(fitness) - 1}
+#' and the selection process quits with an error message if \code{n.select} is greater
+#' than 1.
 #'
 #' @template arg_fitness
 #' @template arg_n_select
@@ -14,12 +20,20 @@
 #' @export
 selDomHV = makeSelector(
   selector = function(fitness, n.select, ref.point) {
-    assertNumeric(ref.point)
-    all.idx = 1:ncol(fitness)
+    checkmate::assertMatrix(fitness, mode = "numeric", min.rows = 2L, min.cols = 2L, any.missing = FALSE, all.missing = FALSE)
+    checkmate::assertNumeric(ref.point, any.missing = FALSE, all.missing = FALSE)
+
+    n = ncol(fitness)
+    if (n.select != (n - 1)) {
+      BBmisc::stopf("[ecr::selDomHV] Note that selDomHV always drops a single individual
+        and hence (ncol(fitness) - 1) individuals are selected. Your choice of
+        n.select (= %i) is not allowed since (ncol(fitness) - 1) equals %i. Check your setup!", n.select, (n - 1L))
+    }
+
+    all.idx = seq_len(n)
 
     # do non-dominated sorting
-    nds.res = doNondominatedSorting(fitness)
-    ranks = nds.res$ranks
+    ranks = doNondominatedSorting(fitness)$ranks
     idx.max = which(ranks == max(ranks))
 
     # there is exactly one individual that is "maximally" dominated

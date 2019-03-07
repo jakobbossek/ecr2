86095819b3b6cae278b4f7ab95697dcbaf000f82
thllwg <t_hell07@uni-muenster.de>
2019-03-07 11:56:46 +0100

    Closes #92, extended explanation on eta parameter

--- R/operator.mutator.polynomial.R
+++ R/operator.mutator.polynomial.R
@@ -3,13 +3,22 @@
 #'
 #' @description
 #' Performs an polynomial mutation as used in the SMS-EMOA algorithm.
+#' Polynomial mutation tries to simulate the distribution of the offspring of 
+#' binary-encoded bit flip mutations based on real-valued decision variables. 
+#' Polynomial mutation favors offspring nearer to the parent. 
+#' 
+#' @references
+#' [1] Deb, Kalyanmoy & Goyal, Mayank. (1999). A Combined Genetic Adaptive 
+#' Search (GeneAS) for Engineering Design. Computer Science and Informatics. 26.
+#' Retrieved from http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.27.767&rep=rep1&type=pdf
 #'
 #' @param ind [\code{numeric}]\cr
 #'   Numeric vector / individual to mutate.
 #' @param p [\code{numeric(1)}]\cr
 #'   Probability of mutation of each gene.
 #' @param eta [\code{numeric(1)}\cr
-#'   Distance parameter of the mutation distribution.
+#'   Distance parameter to control the shape of the mutation distribution. 
+#'   Larger values generate offspring closer to the parents.
 #' @param lower [\code{numeric}]\cr
 #'   Vector of minimal values for each parameter of the decision space.
 #' @param upper [\code{numeric}]\cr

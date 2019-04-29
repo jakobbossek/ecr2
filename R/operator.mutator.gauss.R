258c94cf9da6b3214cf6d18c4362da7fcd5729e9
thllwg <t_hell07@uni-muenster.de>
2019-04-29 11:11:43 +0200

    #83

--- R/operator.mutator.gauss.R
+++ R/operator.mutator.gauss.R
@@ -7,6 +7,14 @@
 #' an individual \eqn{\mathbf{x} \in R^l} this mutator adds a Gaussian
 #' distributed random value to each component of \eqn{\mathbf{x}}, i.~e.,
 #' \eqn{\tilde{\mathbf{x}}_i = \mathbf{x}_i + \sigma \mathcal{N}(0, 1)}.
+#' 
+#' @references 
+#' [1] Beyer, Hans-Georg & Schwefel, Hans-Paul (2002). Evolution strategies. 
+#' Kluwer Academic Publishers.
+#' 
+#' [2] Mateo, P. M. & Alberto, I. (2011). A mutation operator based
+#' on a Pareto ranking for multi-objective evolutionary algorithms. 
+#' Springer Science+Business Meda. 57.
 #'
 #' @param ind [\code{numeric}]\cr
 #'   Numeric vector / individual to mutate.
